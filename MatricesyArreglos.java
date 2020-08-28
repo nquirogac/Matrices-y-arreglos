ca4041268fc45d783a82c62bf4990d40fdf50b8b
nquirogac <69173681+nquirogac@users.noreply.github.com>
2020-08-27 21:16:43 -0500

    Update MatricesyArreglos.java

--- MatricesyArreglos.java
+++ MatricesyArreglos.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package matricesyarreglos;
 
 /**
@@ -11,9 +6,6 @@ package matricesyarreglos;
  */
 public class MatricesyArreglos {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         //Ejercicio 1
         System.out.println("Ejercicio 1 \n");
